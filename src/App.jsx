c61cdd62902a8b74cf80f6b423be3e9b8872c0b2
Dhruv <dhruv@Dhruvs-MacBook-Air.local>
2026-09-15 13:46:54 +0530

    Update website

--- src/App.jsx
+++ src/App.jsx
@@ -837,7 +837,7 @@ function Story() {
           <p>
             We had never believed in love at first sight{' '}
             <br />
-            untill we met each other.
+            until we met each other.
           </p>
 
           <p>
